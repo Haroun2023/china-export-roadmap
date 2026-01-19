0e04ed0a08f0b3f8392162a15eb1041c272fe2da
Haroun2023 <117118785+Haroun2023@users.noreply.github.com>
2026-01-19 16:13:02 -0500

    Update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/china-export-roadmap/',
   plugins: [react()],
   resolve: {
     alias: {
